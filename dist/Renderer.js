ea763b0a0e58b328535f2cb2ea5150f826f00cdd
Yonatan Sharoni <yonatan.sharoni@gmail.com>
2019-07-04 13:20:10 +0300

    finished Renderer

---
+++ dist/Renderer.js
@@ -0,0 +1,28 @@
+class Renderer{
+    render(data){
+        $('div').empty()
+     /*    let source = $('#entry-template').html()
+        let template = Handlebars.compile(source)
+
+        let compiled = template(data)
+        $('.data').append(compiled) */
+        for(let d of data){
+        $('.data').append(`
+         <div class ='title'>
+       <a href= '${d.href}'> ${d.title} </a>
+       <br><br>
+       <img src= '${d.thumbnail}'>
+          <br><br>
+          </div>
+        `)
+        for(let i of d.ingredients){
+        $('.title').append(`
+         <ul class= 'ingredients'>
+         <li>${i}</li>
+         </ul>
+        `)
+        }
+        }
+    }
+}
+
--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,13 @@ $('button').click(function () {
     $.get(`/recipes/${ingredient}`)
         .then(function (res) {
             let data = []
-            for(let r of res){
-                data.push(r.title,r.ingredients, r.thumbnail, r.href)
+            for (let r of res) {
+                data.push({ title: r.title, ingredients: r.ingredients, thumbnail: r.thumbnail, href: r.href })
             }
-            
+
             console.log(data)
+            const renderer = new Renderer
+            renderer.render(data)
+
         })
 })
